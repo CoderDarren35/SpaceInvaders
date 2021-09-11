b9b988286bd33f23c8dff7bc87e8f0ffb37944ef
CoderDarren35 <darrenko23@gmail.com>
2021-09-10 18:01:26 -0700

    adding boss

--- src/game.gd
+++ src/game.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 onready var Swarm = preload("res://src/swarm.tscn")
-
+export var min_boss_delay := 15.0
+export var max_boss_delay := 30.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	randomize()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,6 +18,7 @@ func _on_swarm_lose():
 
 func _on_swarm_win(last_swarm):
 	var swarm = Swarm.instance()
+	swarm.connect("win", self, "_on_swarm_win")
 	swarm.speed = last_swarm.speed * 1.5
 	add_child(swarm)
 	#get_tree().change_scene("res://src/gamewin.tscn")
